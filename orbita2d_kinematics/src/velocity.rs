4a9001db1ec1ae55f702a12cb21b006a273b2ea9
Pierre Rouanet <pierre.rouanet@gmail.com>
2023-07-17 12:01:18 +0200

    Update kinematics with ring/center.

--- orbita2d_kinematics/src/velocity.rs
+++ orbita2d_kinematics/src/velocity.rs
@@ -6,9 +6,9 @@ impl Orbita2dKinematicsModel {
     /// Compute the forward velocity
     ///
     /// # Arguments
-    /// * input_velocity - Input velocity on each motor
+    /// * input_velocity - Input velocity on each motor (motor a, motor b)
     /// # Returns
-    /// * Output oriented velocity
+    /// * Output oriented velocity (ring, center)
     pub fn compute_output_velocity(&self, input_velocity: [f64; 2]) -> [f64; 2] {
         let input_velocity = Vector2f64::from_row_slice(&input_velocity);
         let res = self.inv_mat * input_velocity;
@@ -17,9 +17,9 @@ impl Orbita2dKinematicsModel {
     /// Compute the inverse velocity
     ///
     /// # Arguments
-    /// * output_velocity - target output oriented velocity
+    /// * output_velocity - target output oriented velocity (ring, center)
     /// # Returns
-    /// * Necessary input velocity on each motor
+    /// * Necessary input velocity on each motor (motor a, motor b)
     pub fn compute_input_velocity(&self, output_velocity: [f64; 2]) -> [f64; 2] {
         let output_velocity = Vector2f64::from_row_slice(&output_velocity);
         let res = self.mat * output_velocity;
