f5e0cf1d95c967e5c09980ab799a3c8095f4f779
Pierre Rouanet <pierre.rouanet@gmail.com>
2023-07-17 15:56:50 +0200

    Fix python binding.

--- orbita2d_c_api/python/orbita2d/__init__.py
+++ orbita2d_c_api/python/orbita2d/__init__.py
@@ -2,84 +2,84 @@ from . import _orbita2d
 
 
 class KinematicsModel:
-    def __init__(self, ratio_a, ratio_b) -> None:
+    def __init__(self, ratio_a: float, ratio_b: float) -> None:
         self.ratio_a = ratio_a
         self.ratio_b = ratio_b
 
-    def forward_position(self, angle_a, angle_b):
-        roll = _orbita2d.ffi.new("double *")
-        pitch = _orbita2d.ffi.new("double *")
+    def forward_position(self, angle_a: float, angle_b: float) -> (float, float):
+        ring = _orbita2d.ffi.new("double *")
+        center = _orbita2d.ffi.new("double *")
 
         _orbita2d.lib.orbita2d_forward_position(
-            self.ratio_a, self.ratio_b, angle_a, angle_b, roll, pitch
+            self.ratio_a, self.ratio_b, angle_a, angle_b, ring, center
         )
 
-        return roll[0], pitch[0]
+        return ring[0], center[0]
 
-    def forward_velocity(self, velocity_a, velocity_b):
-        roll_velocity = _orbita2d.ffi.new("double *")
-        pitch_velocity = _orbita2d.ffi.new("double *")
+    def forward_velocity(self, velocity_a: float, velocity_b: float) -> (float, float):
+        ring_velocity = _orbita2d.ffi.new("double *")
+        center_velocity = _orbita2d.ffi.new("double *")
 
         _orbita2d.lib.orbita2d_forward_velocity(
             self.ratio_a,
             self.ratio_b,
             velocity_a,
             velocity_b,
-            roll_velocity,
-            pitch_velocity,
+            ring_velocity,
+            center_velocity,
         )
 
-        return roll_velocity[0], pitch_velocity[0]
+        return ring_velocity[0], center_velocity[0]
 
-    def forward_torque(self, torque_a, torque_b):
-        roll_torque = _orbita2d.ffi.new("double *")
-        pitch_torque = _orbita2d.ffi.new("double *")
+    def forward_torque(self, torque_a: float, torque_b: float) -> (float, float):
+        ring_torque = _orbita2d.ffi.new("double *")
+        center_torque = _orbita2d.ffi.new("double *")
 
         _orbita2d.lib.orbita2d_forward_torque(
             self.ratio_a,
             self.ratio_b,
             torque_a,
             torque_b,
-            roll_torque,
-            pitch_torque,
+            ring_torque,
+            center_torque,
         )
 
-        return roll_torque[0], pitch_torque[0]
+        return ring_torque[0], center_torque[0]
 
-    def inverse_position(self, roll, pitch):
+    def inverse_position(self, ring: float, center: float) -> (float, float):
         angle_a = _orbita2d.ffi.new("double *")
         angle_b = _orbita2d.ffi.new("double *")
 
         _orbita2d.lib.orbita2d_inverse_position(
-            self.ratio_a, self.ratio_b, roll, pitch, angle_a, angle_b
+            self.ratio_a, self.ratio_b, ring, center, angle_a, angle_b
         )
 
         return angle_a[0], angle_b[0]
 
-    def inverse_velocity(self, roll_velocity, pitch_velocity):
+    def inverse_velocity(self, ring_velocity: float, center_velocity: float) -> (float, float):
         velocity_a = _orbita2d.ffi.new("double *")
         velocity_b = _orbita2d.ffi.new("double *")
 
         _orbita2d.lib.orbita2d_inverse_velocity(
             self.ratio_a,
             self.ratio_b,
-            roll_velocity,
-            pitch_velocity,
+            ring_velocity,
+            center_velocity,
             velocity_a,
             velocity_b,
         )
 
         return velocity_a[0], velocity_b[0]
 
-    def inverse_torque(self, roll_torque, pitch_torque):
+    def inverse_torque(self, ring_torque: float, center_torque: float) -> (float, float):
         torque_a = _orbita2d.ffi.new("double *")
         torque_b = _orbita2d.ffi.new("double *")
 
         _orbita2d.lib.orbita2d_inverse_torque(
             self.ratio_a,
             self.ratio_b,
-            roll_torque,
-            pitch_torque,
+            ring_torque,
+            center_torque,
             torque_a,
             torque_b,
         )
@@ -92,31 +92,9 @@ class Orbita2dController:
         self.uid = uid
 
     @classmethod
-    def with_flipsky_serial(
-        cls,
-        serial_ports: tuple[str, str],
-        ids: tuple[int, int],
-        offsets: tuple[float, float],
-        ratios: tuple[float, float],
-        limits: tuple[tuple[float, float], tuple[float, float]],
-    ) -> "Orbita2dController":
-        uid = _orbita2d.ffi.new("int *")
-
-        _orbita2d.lib.orbita2d_controller_with_flipsky_serial(
-            serial_ports[0],
-            serial_ports[1],
-            ids[0],
-            ids[1],
-            offsets[0],
-            offsets[1],
-            ratios[0],
-            ratios[1],
-            limits[0][0],
-            limits[0][1],
-            limits[1][0],
-            limits[1][1],
-            uid,
-        )
+    def from_config(cls, configfile: str):
+        uid = _orbita2d.ffi.new("uint32_t *")
+        _orbita2d.lib.orbita2d_controller_from_config(configfile.encode(), uid)
 
         return cls(uid[0])
 
@@ -125,57 +103,75 @@ class Orbita2dController:
         _orbita2d.lib.orbita2d_is_torque_on(self.uid, on)
         return on[0]
 
-    def enable_torque(self, reset_target: bool = True):
+    def enable_torque(self, reset_target: bool):
         _orbita2d.lib.orbita2d_enable_torque(self.uid, reset_target)
 
     def disable_torque(self):
         _orbita2d.lib.orbita2d_disable_torque(self.uid)
 
-    def get_current_orientation(self) -> tuple[float, float]:
-        orientation = _orbita2d.ffi.new("double [2]")
-        _orbita2d.lib.orbita2d_get_current_orientation(self.uid, orientation)
-        return orientation
+    def get_current_orientation(self) -> (float, float):
+        angle = _orbita2d.ffi.new("double(*)[2]")
+        _orbita2d.lib.orbita2d_get_current_orientation(self.uid, angle)
+        return angle[0][0], angle[0][1]
 
-    def get_current_velocity(self) -> tuple[float, float]:
-        velocity = _orbita2d.ffi.new("double [2]")
+    def get_current_velocity(self) -> (float, float):
+        velocity = _orbita2d.ffi.new("double(*)[2]")
         _orbita2d.lib.orbita2d_get_current_velocity(self.uid, velocity)
-        return velocity
+        return velocity[0][0], velocity[0][1]
 
-    def get_current_torque(self) -> tuple[float, float]:
-        torque = _orbita2d.ffi.new("double [2]")
+    def get_current_torque(self) -> (float, float):
+        torque = _orbita2d.ffi.new("double(*)[2]")
         _orbita2d.lib.orbita2d_get_current_torque(self.uid, torque)
-        return torque
+        return torque[0][0], torque[0][1]
 
-    def get_target_orientation(self) -> tuple[float, float]:
-        target = _orbita2d.ffi.new("double [2]")
-        _orbita2d.lib.orbita2d_get_target_orientation(self.uid, target)
-        return target
+    def get_target_orientation(self) -> (float, float):
+        angle = _orbita2d.ffi.new("double(*)[2]")
+        _orbita2d.lib.orbita2d_get_target_orientation(self.uid, angle)
+        return angle[0][0], angle[0][1]
 
-    def set_target_orientation(self, target: tuple[float, float]):
-        _orbita2d.lib.orbita2d_set_target_orientation(self.uid, target)
+    def set_target_orientation(self, target: (float, float)):
+        arr = _orbita2d.ffi.new("double(*)[2]", target)
+        arr[0][0] = target[0]
+        arr[0][1] = target[1]
+        _orbita2d.lib.orbita2d_set_target_orientation(self.uid, arr)
 
-    def get_velocity_limit(self) -> tuple[float, float]:
-        velocity_limit = _orbita2d.ffi.new("double [2]")
-        _orbita2d.lib.orbita2d_get_velocity_limit(self.uid, velocity_limit)
-        return velocity_limit
+    def get_raw_motors_velocity_limit(self) -> (float, float):
+        limit = _orbita2d.ffi.new("double(*)[2]")
+        _orbita2d.lib.orbita2d_get_raw_motors_velocity_limit(self.uid, limit)
+        return limit[0][0], limit[0][1]
 
-    def set_velocity_limit(self, limit: tuple[float, float]):
-        _orbita2d.lib.orbita2d_set_velocity_limit(self.uid, limit)
+    def set_raw_motors_velocity_limit(self, limit: (float, float)):
+        arr = _orbita2d.ffi.new("double(*)[2]", limit)
+        arr[0][0] = limit[0]
+        arr[0][1] = limit[1]
+        _orbita2d.lib.orbita2d_set_raw_motors_velocity_limit(self.uid, arr)
 
-    def get_torque_limit(self) -> tuple[float, float]:
-        torque_limit = _orbita2d.ffi.new("double [2]")
-        _orbita2d.lib.orbita2d_get_torque_limit(self.uid, torque_limit)
-        return torque_limit
+    def get_raw_motors_torque_limit(self) -> (float, float):
+        limit = _orbita2d.ffi.new("double(*)[2]")
+        _orbita2d.lib.orbita2d_get_raw_motors_torque_limit(self.uid, limit)
+        return limit[0][0], limit[0][1]
 
-    def set_torque_limit(self, limit: tuple[float, float]):
-        _orbita2d.lib.orbita2d_set_torque_limit(self.uid, limit)
+    def set_raw_motors_torque_limit(self, limit: (float, float)):
+        arr = _orbita2d.ffi.new("double(*)[2]", limit)
+        arr[0][0] = limit[0]
+        arr[0][1] = limit[1]
+        _orbita2d.lib.orbita2d_set_raw_motors_torque_limit(self.uid, arr)
 
-    def get_pid_gains(self) -> tuple[float, float, float]:
-        kp = _orbita2d.ffi.new("double *")
-        ki = _orbita2d.ffi.new("double *")
-        kd = _orbita2d.ffi.new("double *")
-        _orbita2d.lib.orbita2d_get_pid_gains(self.uid, kp, ki, kd)
-        return kp[0], ki[0], kd[0]
+    def get_raw_motors_pid_gains(
+        self,
+    ) -> ((float, float, float), (float, float, float)):
+        pid = _orbita2d.ffi.new("double(*)[6]")
+        _orbita2d.lib.orbita2d_get_raw_motors_pid_gains(self.uid, pid)
+        return (pid[0][0], pid[0][1], pid[0][2]), (pid[0][3], pid[0][4], pid[0][5])
 
-    def set_pid_gains(self, gains: tuple[float, float, float]):
-        _orbita2d.lib.orbita2d_set_pid_gains(self.uid, gains[0], gains[1], gains[2])
+    def set_raw_motors_pid_gains(
+        self, pid: ((float, float, float), (float, float, float))
+    ):
+        arr = _orbita2d.ffi.new("double(*)[6]")
+        arr[0][0] = pid[0][0]
+        arr[0][1] = pid[0][1]
+        arr[0][2] = pid[0][2]
+        arr[0][3] = pid[1][0]
+        arr[0][4] = pid[1][1]
+        arr[0][5] = pid[1][2]
+        _orbita2d.lib.orbita2d_set_raw_motors_pid_gains(self.uid, arr)
