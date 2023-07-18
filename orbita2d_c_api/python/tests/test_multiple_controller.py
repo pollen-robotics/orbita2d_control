544958a50baef238c24d1326e9435c8736bdf1ca
Pierre Rouanet <pierre.rouanet@gmail.com>
2023-07-18 16:42:20 +0200

    Add multiple controller tests.

---
+++ orbita2d_c_api/python/tests/test_multiple_controller.py
@@ -0,0 +1,13 @@
+from . import get_fake_controller
+
+
+def test_multiple_controller():
+    controllers = [get_fake_controller() for _ in range(10)]
+    uids = [controller.uid for controller in controllers]
+    assert len(set(uids)) == len(uids)
+
+    for i, controller in enumerate(controllers):
+        controller.set_target_orientation((i, i))
+
+    for i, controller in enumerate(controllers):
+        assert controller.get_target_orientation() == (i, i)    
