119528078ad5718931389e9e42df3d8f0f3f21cf
Pierre Rouanet <pierre.rouanet@gmail.com>
2023-10-02 15:31:40 +0200

    Update hwi to match actuator.

--- orbita2d_system_hwi/src/orbita2d_system_hwi.cpp
+++ orbita2d_system_hwi/src/orbita2d_system_hwi.cpp
@@ -102,6 +102,9 @@ Orbita2dSystem::on_activate(const rclcpp_lifecycle::State & /*previous_state*/)
 {
   // Set some default values
   auto ret= CallbackReturn::SUCCESS;
+
+  hw_states_torque_ = std::numeric_limits<double>::quiet_NaN();
+
   for (int i=0; i < 2; i++) {
     hw_states_position_[i] = std::numeric_limits<double>::quiet_NaN();
     hw_states_velocity_[i] = std::numeric_limits<double>::quiet_NaN();
@@ -109,7 +112,6 @@ Orbita2dSystem::on_activate(const rclcpp_lifecycle::State & /*previous_state*/)
     hw_states_temperature_[i] = std::numeric_limits<double>::quiet_NaN();
     hw_states_torque_limit_[i] = std::numeric_limits<double>::quiet_NaN();
     hw_states_speed_limit_[i] = std::numeric_limits<double>::quiet_NaN();
-    hw_states_torque_[i] = std::numeric_limits<double>::quiet_NaN();
     hw_states_p_gain_[i] = std::numeric_limits<double>::quiet_NaN();
     hw_states_i_gain_[i] = std::numeric_limits<double>::quiet_NaN();
     hw_states_d_gain_[i] = std::numeric_limits<double>::quiet_NaN();
@@ -133,12 +135,8 @@ Orbita2dSystem::on_activate(const rclcpp_lifecycle::State & /*previous_state*/)
     );
     ret= CallbackReturn::ERROR;
   }
-  hw_commands_torque_[0] = torque_on;
-  hw_commands_torque_[1] = torque_on;
-
-  hw_states_torque_[0] = torque_on;
-  hw_states_torque_[1] = torque_on;
-
+  hw_commands_torque_ = torque_on ? 1.0 : 0.0;
+  hw_states_torque_ = torque_on ? 1.0 : 0.0;
 
   //init the states to the read values
 
@@ -220,11 +218,12 @@ Orbita2dSystem::on_activate(const rclcpp_lifecycle::State & /*previous_state*/)
   }
 
   //set the commands to the read values (otherwise some strange behaviour can happen)
+  hw_commands_torque_ = hw_states_torque_;
+
   for (int i=0; i < 2; i++) {
     hw_commands_position_[i]=hw_states_position_[i];
     hw_commands_torque_limit_[i]=hw_states_torque_limit_[i];
     hw_commands_speed_limit_[i]=hw_states_speed_limit_[i];
-    hw_commands_torque_[i]=hw_states_torque_[i];
     hw_commands_p_gain_[i]=hw_states_p_gain_[i];
     hw_commands_i_gain_[i]=hw_states_i_gain_[i];
     hw_commands_d_gain_[i]=hw_states_d_gain_[i];
@@ -258,6 +257,9 @@ Orbita2dSystem::export_state_interfaces()
 {
   std::vector<hardware_interface::StateInterface> state_interfaces;
 
+  state_interfaces.emplace_back(hardware_interface::StateInterface(
+    info_.name.c_str(), "torque", &hw_states_torque_));
+
   for (std::size_t i = 0; i < 2; i++)
   {
     auto joint = info_.joints[i];
@@ -274,8 +276,6 @@ Orbita2dSystem::export_state_interfaces()
       joint.name, "torque_limit", &hw_states_torque_limit_[i]));
     state_interfaces.emplace_back(hardware_interface::StateInterface(
       joint.name, "speed_limit", &hw_states_speed_limit_[i]));
-    state_interfaces.emplace_back(hardware_interface::StateInterface(
-      joint.name, "torque", &hw_states_torque_[i]));
     state_interfaces.emplace_back(hardware_interface::StateInterface(
       joint.name, "p_gain", &hw_states_p_gain_[i]));
     state_interfaces.emplace_back(hardware_interface::StateInterface(
@@ -297,6 +297,9 @@ Orbita2dSystem::export_command_interfaces()
 {
   std::vector<hardware_interface::CommandInterface> command_interfaces;
 
+  command_interfaces.emplace_back(hardware_interface::CommandInterface(
+    info_.name.c_str(), "torque", &hw_commands_torque_));
+
   for (std::size_t i = 0; i < 2; i++)
   {
     auto joint = info_.joints[i];
@@ -307,8 +310,6 @@ Orbita2dSystem::export_command_interfaces()
       joint.name, "speed_limit", &hw_commands_speed_limit_[i]));
     command_interfaces.emplace_back(hardware_interface::CommandInterface(
       joint.name, "torque_limit", &hw_commands_torque_limit_[i]));
-    command_interfaces.emplace_back(hardware_interface::CommandInterface(
-      joint.name, "torque", &hw_commands_torque_[i]));
     command_interfaces.emplace_back(hardware_interface::CommandInterface(
       joint.name, "p_gain", &hw_commands_p_gain_[i]));
     command_interfaces.emplace_back(hardware_interface::CommandInterface(
@@ -355,8 +356,7 @@ Orbita2dSystem::read(const rclcpp::Time &, const rclcpp::Duration &)
       "(%s) Error getting torque status!",info_.name.c_str()
     );
   }
-  hw_states_torque_[0] = torque_on;
-  hw_states_torque_[1] = torque_on;
+  hw_states_torque_ = torque_on ? 1.0 : 0.0;
 
 
   //Velocity
@@ -451,7 +451,7 @@ Orbita2dSystem::write(const rclcpp::Time &, const rclcpp::Duration &)
 
   //We only change torque for both axes
 
-  bool torque= (hw_commands_torque_[0]==1.0 && hw_commands_torque_[1]==1.0);
+  bool torque = (hw_commands_torque_ != 0.0);
   //TODO: we can go with orbita2d_set_raw_torque_limit
   if(torque)
   {
